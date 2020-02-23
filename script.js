f95b5918ebd6741e2a7929265b0fa4ebd84777ab
tamsay <longetemitope@gmail.com>
2020-02-23 20:59:13 +0100

    I made the search input fully functional with Javascript. I'll keep on making those changes till it becomes a fully functional website

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log('It is working')
+let search = document.querySelector('#search');
+let sportList = document.querySelectorAll('#programmeGrid div')
+let displayDiv = document.querySelector('#programmeGrid')
+
+let createSportGrid =(array)=>{
+    for(x=0; x < array.length; x++){
+        displayDiv.appendChild(array[x]);
+    }
+}
+
+
+let displaySport=()=>{
+    displayDiv.innerText = "";
+    let filteredArray = [];
+    let input = search.value;
+
+    [...sportList].map(element=>{
+        
+        if (element.textContent.toLowerCase().includes(input.toLowerCase())){
+            filteredArray.push(element);
+        }
+    })
+    createSportGrid(filteredArray);
+}
+
+
+search.addEventListener('input' , displaySport)
